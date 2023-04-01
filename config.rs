8cb17bb424d15450c195360374c3dfc17f54eeff
filipton <filipton12@gmail.com>
2023-04-01 20:30:16 +0200

    chore: removed unused code

--- config.rs
+++ config.rs
@@ -9,32 +9,11 @@ pub struct BuildrsConfig {
     pub tmux_single_window: bool,
 }
 
-impl Default for BuildrsConfig {
-    fn default() -> Self {
-        Self {
-            filenames_as_scopes: false,
-            disable_scopes: false,
-            tmux_single_window: true,
-        }
-    }
-}
-
 impl BuildrsConfig {
-    pub fn new() -> Self {
-        Self::default()
-    }
-
     pub fn from_file(path: PathBuf) -> Result<Self> {
         let file = std::fs::read_to_string(path)?;
         let config = serde_json::from_str(&file)?;
 
         Ok(config)
-    }
-
-    pub fn to_file(data: Self, path: PathBuf) -> Result<()> {
-        let config = serde_json::to_string_pretty(&data)?;
-        std::fs::write(path, config)?;
-
-        Ok(())
     }
 }
