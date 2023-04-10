72073a4119a65bdbff5e1700302a0a558367fd34
filipton <filipton12@gmail.com>
2023-04-10 23:04:25 +0200

    feat(actix): use of fnstack build utils

--- Actix/build.rs
+++ Actix/build.rs
@@ -2,26 +2,18 @@ const PAGES_DIR: &'static str = "src/pages";
 const SCOPE_PATH: &'static str = "src/actix_scope.rs";
 
 use anyhow::Result;
+use fnstack_build_utils::PageEntry;
 use rust_format::{Formatter, RustFmt};
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
-use syn::ItemFn;
-
-#[derive(Debug, Clone)]
-struct PageEntry {
-    name: String,
-    is_dir: bool,
-    children: Vec<PageEntry>,
-}
 
 fn main() -> Result<()> {
     let config: BuildrsConfig = BuildrsConfig::from_file(PathBuf::from("fnstack.json"))?;
-
     let pages = PathBuf::from(PAGES_DIR);
 
-    let entries: PageEntry = PageEntry::generate(pages, &config)?;
-    let lib_str = entries.generate_mods()?;
-    let services_str = entries.generate_services(&config);
+    let entries: PageEntry = PageEntry::generate(&pages)?;
+    let mods_str = entries.get_mods_string()?;
+    let services_str = generate_services(&entries, &config);
 
     let services = format!(
         r#"
@@ -42,7 +34,7 @@ fn main() -> Result<()> {
 
             {}
         "#,
-        lib_str, services
+        mods_str, services
     );
 
     main_template_content = RustFmt::new().format_str(main_template_content)?;
@@ -55,162 +47,67 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-impl PageEntry {
-    pub fn generate(dir: PathBuf, config: &BuildrsConfig) -> Result<PageEntry> {
-        let mut children: Vec<PageEntry> = Vec::new();
+// TODO: Refactor this
+pub fn generate_services(entry: &PageEntry, config: &BuildrsConfig) -> String {
+    let mut tmp = String::from("web::scope(\"\")");
 
-        for entry in dir.read_dir()? {
-            if let Ok(entry) = entry {
-                let file_type = entry.file_type()?;
-
-                if file_type.is_dir() {
-                    children.push(PageEntry::generate(entry.path(), config)?);
-                } else if file_type.is_file() {
-                    let file_name = entry
-                        .file_name()
-                        .to_str()
-                        .unwrap()
-                        .split('.')
-                        .collect::<Vec<&str>>()[0]
-                        .to_owned();
-
-                    children.push(PageEntry {
-                        name: file_name,
-                        is_dir: entry.file_type()?.is_dir(),
-                        children: vec![],
-                    })
-                }
-            }
+    for child in &entry.children {
+        if child.children.len() > 0 {
+            tmp += &_generate_services(&child, PathBuf::from("src/pages"), config);
+            continue;
         }
 
-        // WTF???
-        let dir_name = dir.file_name().unwrap().to_str().unwrap().to_owned();
-        children.sort_by_key(|k| k.is_dir);
+        let child_path = PathBuf::from(PAGES_DIR).join(format!("{}.rs", child.name));
 
-        return Ok(PageEntry {
-            name: dir_name,
-            is_dir: true,
-            children,
-        });
+        for route in fnstack_build_utils::get_file_routes(child_path).unwrap_or(vec![]) {
+            tmp += &format!(
+                ".service(pages::{}::{})\n",
+                child.name.replace("{", "_").replace("}", "_"),
+                route.function
+            );
+        }
     }
 
-    pub fn generate_mods(&self) -> Result<String> {
-        let mut out = String::new();
-        if self.is_dir && self.children.len() == 0 {
-            return Ok(out);
-        }
+    return tmp;
+}
 
-        if self.name.contains("{") || self.name.contains("}") {
-            out += &format!("#[path = \"{}\"]\n", self.name);
-        }
-        out += &format!("pub mod {}", self.name.replace("{", "_").replace("}", "_"));
-        if self.children.len() > 0 {
-            out += "{ \n";
+fn _generate_services(entry: &PageEntry, path: PathBuf, config: &BuildrsConfig) -> String {
+    let mut tmp = String::new();
+    let path = path.join(&entry.name);
 
-            for child in self.children.clone() {
-                out += &child.generate_mods()?;
-            }
-
-            out += "} \n";
-        } else {
-            out += "; \n";
-        }
-
-        Ok(out)
+    if !config.disable_scopes {
+        tmp += &format!(".service(web::scope(\"{}\")\n", entry.name);
     }
 
-    pub fn generate_services(&self, config: &BuildrsConfig) -> String {
-        let mut tmp = String::from("web::scope(\"\")");
-
-        for child in &self.children {
-            if child.children.len() > 0 {
-                tmp += &*child._generate_services(PathBuf::from("src/pages"), config);
-                continue;
-            }
-
-            let child_path = PathBuf::from(PAGES_DIR).join(format!("{}.rs", child.name));
-            for endpoint in PageEntry::get_actix_endpoints(child_path).unwrap_or(vec![]) {
-                tmp += &format!(
-                    ".service(pages::{}::{})",
-                    child.name.replace("{", "_").replace("}", "_"),
-                    endpoint
-                );
-            }
+    for child in &entry.children {
+        if child.children.len() > 0 {
+            tmp += &_generate_services(&child, path.clone(), config);
+            continue;
         }
 
-        return tmp;
+        let tmp_path = path.clone().join(format!("{}.rs", child.name));
+        let use_path = path
+            .to_str()
+            .unwrap()
+            .replacen("src/", "", 1)
+            .replace("/", "::")
+            .replace("{", "_")
+            .replace("}", "_");
+
+        for route in fnstack_build_utils::get_file_routes(tmp_path).unwrap_or(vec![]) {
+            tmp += &format!(
+                ".service({}::{}::{})\n",
+                use_path,
+                child.name.replace("{", "_").replace("}", "_"),
+                route.function
+            );
+        }
     }
 
-    fn _generate_services(&self, path: PathBuf, config: &BuildrsConfig) -> String {
-        let mut tmp = String::new();
-        let path = path.join(&self.name);
-
-        if !config.disable_scopes {
-            tmp += &format!(".service(web::scope(\"{}\")\n", self.name);
-        }
-
-        for child in self.children.clone() {
-            if child.children.len() > 0 {
-                tmp += &child._generate_services(path.clone(), config);
-                continue;
-            }
-
-            let tmp_path = path.clone().join(format!("{}.rs", child.name));
-            let use_path = path
-                .to_str()
-                .unwrap()
-                .replacen("src/", "", 1)
-                .replace("/", "::")
-                .replace("{", "_")
-                .replace("}", "_");
-
-            for endpoint in PageEntry::get_actix_endpoints(tmp_path).unwrap_or(vec![]) {
-                tmp += &format!(".service({}::{}::{})\n", use_path, child.name, endpoint);
-            }
-        }
-
-        if !config.disable_scopes {
-            tmp += ")";
-        }
-        return tmp;
+    if !config.disable_scopes {
+        tmp += ")";
     }
-
-    pub fn get_actix_endpoints(path: PathBuf) -> Result<Vec<String>> {
-        let file_content = std::fs::read_to_string(path)?;
-
-        let syntax = syn::parse_file(&file_content).unwrap();
-        let functions: Vec<String> = syntax
-            .items
-            .iter()
-            .filter_map(|item| {
-                if let syn::Item::Fn(item_fn) = item {
-                    if PageEntry::is_actix_attr(item_fn) {
-                        return Some(item_fn.sig.ident.to_string());
-                    }
-                }
-
-                None
-            })
-            .collect();
-
-        return Ok(functions);
-    }
-
-    const ACTIX_MACROS: [&'static str; 7] =
-        ["get", "post", "put", "delete", "head", "options", "patch"];
-
-    fn is_actix_attr(item: &ItemFn) -> bool {
-        for attr in item.attrs.clone() {
-            for segment in attr.path.segments {
-                let ident = segment.ident.to_string();
-                if PageEntry::ACTIX_MACROS.contains(&ident.as_str()) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
+    return tmp;
 }
 
 #[derive(Serialize, Deserialize, Debug)]
