644f93dec1d8807eb2390e3b82a166f259021ea9
filipton <filipton12@gmail.com>
2023-05-04 15:48:09 +0200

    fix(cloudflare): planetscale not in if

--- Cloudflare/src/lib.rs
+++ Cloudflare/src/lib.rs
@@ -1,4 +1,3 @@
-use planetscale_driver::PSConnection;
 use worker::*;
 mod router;
 
@@ -7,6 +6,8 @@ async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     return router::route(req, env).await;
 }
 
+//[[IF DATABASE Planetscale]]
+use planetscale_driver::PSConnection;
 pub fn get_db_conn(ctx: &RouteContext<()>) -> Result<PSConnection> {
     let host = ctx.secret("PS_HOST")?.to_string();
     let user = ctx.secret("PS_USER")?.to_string();
@@ -14,3 +15,4 @@ pub fn get_db_conn(ctx: &RouteContext<()>) -> Result<PSConnection> {
 
     return Ok(PSConnection::new(&host, &user, &pass));
 }
+//[[ENDIF]]
